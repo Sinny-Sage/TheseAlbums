eb31a19887138e695205dc16c1d3f2134da4cab5
Sintra Olivia Saenz <thatblush@gmail.com>
2022-11-07 16:56:37 -0600

    new layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css'
-import data from './data.json'
+
+import Data from './data.json'
+
 const App = () => {
   return (
-    <div>
+    <div className="App">
       <h1>Sinny's Indie Boxs</h1>
+      <div>
+        {Data.map((data) => {
+          return <div className="box">{data.title}</div>
+        })}
+      </div>
     </div>
   )
 }
